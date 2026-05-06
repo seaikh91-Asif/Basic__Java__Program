5ce4de88880e50f8e0e407650e6bb8c84c944766
seaikh91-Asif <seaikh91@gmail.com>
2026-05-06 21:28:22 +0530

    printing number from one to n

---
+++ src/PrintNumber_from_One_to_n.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class PrintNumber_from_One_to_n {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        // for taking input from the user
+        System.out.print("Enter the number till you want to print: ");
+        int n = input.nextInt();
+
+        // the main logic part
+        // and alos the output part
+        for (int i = 0; i <= n; i++){
+            System.out.println(i);
+        }
+    }
+}
