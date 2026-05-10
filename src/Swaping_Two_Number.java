69004de42e1fc105cb86ba466fd7ed8727dbd604
seaikh91-Asif <seaikh91@gmail.com>
2026-05-10 11:41:11 +0530

    Swappint version 1.1

--- src/Swaping_Two_Number.java
+++ src/Swaping_Two_Number.java
@@ -3,20 +3,20 @@ import java.util.Scanner;
 public class Swaping_Two_Number {
     public static void main(String[] args){
 
-        // taking input from the user
+        // teking input from the user
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter first Number: ");
-        int first = input.nextInt();
-        System.out.println("Enter Second Number: ");
-        int second = input.nextInt();
+        System.out.print("enter your first number: ");
+        int fn = input.nextInt();
+        System.out.print("enter your second number: ");
+        int sn = input.nextInt();
 
-        // the main logic part
-        int temp = first;
-        first = second;
-        second = temp;
+        // the main logic part of the program
+        int temp = fn;
+        fn = sn;
+        sn = temp;
 
-        //the output part
-        System.out.println("Swapped firstNumber = " + first);
-        System.out.println("Swapped secondNumber = " + second);
+        // printing numbers:
+        System.out.println("swapped first number = " + fn);
+        System.out.println("swapped second number = " + sn);
     }
 }
