074b072a43822ddb92080db2d6849d28949f8887
seaikh91-Asif <seaikh91@gmail.com>
2026-05-13 09:38:42 +0530

    calculator_1.1

--- Programs/Calculator_Lite.java
+++ Programs/Calculator_Lite.java
@@ -5,7 +5,7 @@ public class Calculator_Lite {
 
         Scanner scanner = new Scanner(System.in);
 
-        // ভেরিয়েবল ডিক্লেয়ার করা হলো
+
         int sum, sub, mul, div;
 
         System.out.print("Enter the first number: ");
@@ -14,14 +14,14 @@ public class Calculator_Lite {
         System.out.print("Enter the second number: ");
         int b = scanner.nextInt();
 
-        // Buffer clear করার জন্য ঠিক এইখানে nextLine() দিতে হবে
+
         scanner.nextLine();
 
         System.out.print("Enter which operation you want to perform (addition, subtraction, multiplication, division): ");
         String opt = scanner.nextLine();
 
-        // if-else if লজিক দিয়ে অপারেশনগুলো চেক করা হচ্ছে
-        // .trim() ব্যবহার করা হয়েছে যাতে অতিরিক্ত স্পেস কেটে যায়
+
+
         if (opt.trim().equalsIgnoreCase("addition")) {
             sum = a + b;
             System.out.println("The addition is = " + sum);
@@ -46,7 +46,7 @@ public class Calculator_Lite {
             System.out.println("Invalid operation! Please type addition, subtraction, multiplication, or division.");
         }
 
-        // স্ক্যানার ক্লোজ করা ভালো প্র্যাকটিস
+
         scanner.close();
     }
 }
