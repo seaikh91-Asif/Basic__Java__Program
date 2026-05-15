d58df20a0f2a2948f9af3d71c5119cdcc5fb7f64
seaikh91-Asif <seaikh91@gmail.com>
2026-05-15 21:44:00 +0530

    equation version1.1

--- Programs/Roots_of_quadraticEquation.java
+++ Programs/Roots_of_quadraticEquation.java
@@ -6,7 +6,7 @@ public class Roots_of_quadraticEquation {
 
         System.out.println("Quadratic Equation format: ax^2 + bx + c = 0");
 
-        // a, b, and c er value input neoa
+
         System.out.print("Enter value for a: ");
         double a = scanner.nextDouble();
 
@@ -16,10 +16,10 @@ public class Roots_of_quadraticEquation {
         System.out.print("Enter value for 1c: ");
         double c = scanner.nextDouble();
 
-        // Discriminant (D) calculate kora: D = b^2 - 4ac
+
         double d = (b * b) - (4 * a * c);
 
-        // Condition check kora
+
         if (d > 0) {
             // D > 0 hole duti alada real root thakbe
             double root1 = (-b + Math.sqrt(d)) / (2 * a);
@@ -29,13 +29,13 @@ public class Roots_of_quadraticEquation {
             System.out.println("Root 2 = " + root2);
         }
         else if (d == 0) {
-            // D == 0 hole duti soman real root thakbe
+
             double root = -b / (2 * a);
             System.out.println("\nRootr are real and equal (soman).");
             System.out.println("Root 1 = Root 2 = " + root);
         }
         else {
-            // D < 0 hole kono real root thakbe na
+
             System.out.println("\nReal Roots are doesn't exist (Roots are complex/imaginary).");
         }
 
