4dd155e72f450603fb9e0511b39018da92d7f683
seaikh91-Asif <seaikh91@gmail.com>
2026-05-16 12:06:16 +0530

    prime number version 1.2

--- Programs/PrimeNumber.java
+++ Programs/PrimeNumber.java
@@ -4,7 +4,7 @@ public class PrimeNumber {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter a number that you want to cheach: ");
+        System.out.print("Enter a number that you want to cheack: ");
         int num = scanner.nextInt();
 
         boolean isPrime = true;
