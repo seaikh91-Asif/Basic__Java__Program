e4428ee7d68e21fe852fb365fbc38be11ac2eca3
seaikh91-Asif <seaikh91@gmail.com>
2026-05-17 11:04:55 +0530

    power of two or not in bitwise

--- Programs/PowerOfTwoOrNot_IN_Bitwaise.java
+++ Programs/PowerOfTwoOrNot_IN_Bitwaise.java
@@ -1,5 +1,12 @@
+import java.net.HttpRetryException;
+import java.util.Scanner;
+
 public class PowerOfTwoOrNot_IN_Bitwaise {
     public static void main(String[] args){
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("enter a number: ");
+        int n = scanner.nextInt();
+        boolean ans = (n & (n - 1)) == 0;
+        System.out.println(ans);
     }
 }
